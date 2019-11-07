0c05dc9b4caca94b64d60808b1befae1b647bf04
Elviss Strazdins <elviss@elviss.lv>
2019-11-07 03:40:32 +0200

    Start implementing CoreAudio backend

---
+++ osound/coreaudio/AudioDeviceCA.cpp
@@ -0,0 +1,8 @@
+#include "AudioDeviceCA.hpp"
+
+namespace osound
+{
+    namespace coreaudio
+    {
+    }
+}
---
+++ osound/coreaudio/AudioDeviceCA.hpp
@@ -0,0 +1,15 @@
+#ifndef AUDIODEVICECA_hpp
+#define AUDIODEVICECA_hpp
+
+namespace osound
+{
+    namespace coreaudio
+    {
+        class AudioDevice
+        {
+
+        };
+    }
+}
+
+#endif // AUDIODEVICECA_hpp

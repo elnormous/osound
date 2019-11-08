a5a7881025aa644849032a8db8df4cad4b2be249
Elviss Strazdins <elviss@elviss.lv>
2019-11-08 03:30:57 +0200

    Add SampleFormat enum

---
+++ osound/SampleFormat.hpp
@@ -0,0 +1,13 @@
+#ifndef SAMPLEFORMAT_HPP
+#define SAMPLEFORMAT_HPP
+
+namespace osound
+{
+    enum class SampleFormat
+    {
+        SignedInt16,
+        Float32
+    };
+} // namespace ouzel
+
+#endif // OUZEL_AUDIO_SAMPLEFORMAT_HPP

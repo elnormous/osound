44fafc91867c7e39a9fe7da12d4380d7e139c8ef
Elviss Strazdins <elviss@elviss.lv>
2022-01-19 16:17:49 +0200

    Use nested namespace declaration

--- osound/coreaudio/CAAudioDevice.hpp
+++ osound/coreaudio/CAAudioDevice.hpp
@@ -13,38 +13,35 @@
 
 #include "AudioDevice.hpp"
 
-namespace osound
+namespace osound::coreaudio
 {
-    namespace coreaudio
+    class AudioDevice: public osound::AudioDevice
     {
-        class AudioDevice: public osound::AudioDevice
-        {
-        public:
-            AudioDevice(uint32_t initBufferSize,
-                        uint32_t initSampleRate,
-                        uint16_t initChannels,
-                        const std::function<void(uint32_t frames,
-                                                 uint16_t channels,
-                                                 uint32_t sampleRate,
-                                                 std::vector<float>& samples)>& initDataGetter);
-            ~AudioDevice();
+    public:
+        AudioDevice(uint32_t initBufferSize,
+                    uint32_t initSampleRate,
+                    uint16_t initChannels,
+                    const std::function<void(uint32_t frames,
+                                             uint16_t channels,
+                                             uint32_t sampleRate,
+                                             std::vector<float>& samples)>& initDataGetter);
+        ~AudioDevice();
 
-            void start() final;
-            void stop() final;
+        void start() final;
+        void stop() final;
 
-            void outputCallback(AudioBufferList* ioData);
+        void outputCallback(AudioBufferList* ioData);
 
-        private:
+    private:
 #if TARGET_OS_MAC && !TARGET_OS_IOS && !TARGET_OS_TV
-            AudioDeviceID deviceId = 0;
+        AudioDeviceID deviceId = 0;
 #endif
-            AudioComponent audioComponent = nullptr;
-            AudioUnit audioUnit = nullptr;
+        AudioComponent audioComponent = nullptr;
+        AudioUnit audioUnit = nullptr;
 
-            uint32_t sampleSize = 0;
-            std::vector<uint8_t> data;
-        };
-    }
+        uint32_t sampleSize = 0;
+        std::vector<uint8_t> data;
+    };
 }
 
 #endif // CAAUDIODEVICE_HPP
